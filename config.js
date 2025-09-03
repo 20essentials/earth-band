664c1d3232f638b4ce7ac51e56b8546ec7207ec4
DominicDev <s0l0estudi4r@gmail.com>
2025-09-03 17:18:33 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'EARTH';
+export const nameSongs = [
+	"YOUR SONG", /*1*/
+	"COLOR OF SEASONS", /*2*/
+	"BRAND-NEW DAY", /*3*/
+	"HISTORY OF US", /*4*/
+	"BOYS LIKE THAT", /*5*/
+	"I'M HAPPY WE MET", /*6*/
+	"IMPERAT", /*7*/
+	"WEDDING ROAD", /*8*/
+	"WONDERFUL WORLD", /*9*/
+	"IS THIS LOVE", /*10*/
+	"TIME AFTER TIME", /*11*/
+	"MAKE UP YOUR MIND", /*12*/
+	"IF YOU NEED MY LOVE", /*13*/
+	"IS THIS LOVE", /*14*/
+	"COMMUNICATION", /*15*/
+	"YOU & I", /*16*/
+	"TIME AFTER TIME", /*17*/
+	"EVERYTHING", /*18*/
+	"COLOR OF SEASONS (INSTRUMENTAL)", /*19*/
+	"BRAND-NEW DAY (INSTRUMENTAL)", /*20*/
+]
